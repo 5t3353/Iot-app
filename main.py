8c9abb7e4baa2a857bcf7ef65387dc28fdda4b22
bl4ckc4t <81837200+5t3353@users.noreply.github.com>
2021-11-02 20:11:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
 from flask import Flask , render_template ,request
-import pymysql
-
+from pymongo import MongoClient,DESCENDING
 
 app = Flask(__name__)
 
-msg = "hi"
-      
-      
-data = {'id':[1,2,3,4,5],
-      'temp':[14,15,14,24,10],
-      'hum':[0.5,0.9,1.1,0.09,1],
-      'co2':[0.4,0.6,0.3,0.1,0.8],
-      'datm':["oct 19 13:55",
-             "oct 19 17:40",
-             "oct 19 22:54","oct 20 1:34",
-             "oct 20 5:56"]}
+dir = "mongodb+srv://aqiots_syst:ProtectedPa55W@iot-cluster.zo4ge.mongodb.net/test"
+
+client = MongoClient(dir)
+
+app.database = client.get_database("Iot")
+
+stored_data = [s for s in app.database.sensors.find().sort("_id", DESCENDING).limit(10)]
+
+features = list(stored_data[0].keys())[1:]
+
 
 @app.route('/')
 def principal():
 
-    return render_template("principal.html",msg = msg)
+    return render_template("principal.html")
+
 
 @app.route('/database')
 def database():
 
-    return render_template("data.html",db = data)
+
+    return render_template("data.html" , columns = features,data = stored_data)
+
 
 @app.route('/dashboard')
 def dashboard():
 
-    test_data = [4,6,2]
-
-    return render_template("dashboard.html",data = data)
-
+    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
-  
-  app.run()
 
+    app.run()
